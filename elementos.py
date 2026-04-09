fd47775c9019c231e06fad2e3475301312ba5d28
Edgar Alexander Carrizalez Leŕin <edgar.carrizalez@softtek.com>
2026-04-09 11:32:58 -0600

    Ajustes a project
    Se le agrego los metodos str y repr a project así como una representacion con wrapper a los clusters

--- elementos.py
+++ elementos.py
@@ -3,26 +3,25 @@ import logging
 from google.cloud import container_v1 
 from google.cloud import resourcemanager_v3 as resource_manager
 
-class Proyect:
+class Project:
     def __init__(self , project_id):
         try:
-            self.project = self.open_proyect(project_id)
+            self.project = self.open_project(project_id)
             
         except Exception as e:
-            print(f"Error al abrir proyecto: {project_id}", file=sys.stderr)
-            logging.error(f"Error al abrir proyecto: {project_id}. {e}")
+            print(f"Error al abrir project: {project_id}", file=sys.stderr)
+            logging.error(f"Error al abrir project: {project_id}. {e}")
             sys.exit(1)
 
         try:
             self.clusters = self.extract_clusters(project_id)
-            print(self.clusters)
             
         except Exception as e:
             print(f"Error al buscar clusters", file=sys.stderr)
             logging.error(f"Error al buscar clusters. {e}")
             sys.exit(1)
 
-    def open_proyect(self , project_id):
+    def open_project(self , project_id):
         client = resource_manager.ProjectsClient()
         project = client.get_project(name=f"projects/{project_id}")
         return project
@@ -32,24 +31,44 @@ class Proyect:
         response = client.list_clusters(
             parent=f"projects/{project_id}/locations/-"
         )
-        return response.clusters
+        return [Cluster(cluster) for cluster in response.clusters]
+    
+    def __repr__(self):
+        return (
+            f"Project("
+            f"project_id='{self.project.project_id}', "
+            f"display_name='{self.project.display_name}', "
+            f"state='{self.project.state.name}', "
+            f"clusters={len(self.clusters)}"
+            f")"
+        )
+    
+    def __str__(self):
+        return (
+            f"Proyecto: {self.project.display_name}\n"
+            f"ID: {self.project.project_id}\n"
+            f"Estado: {self.project.state.name}\n"
+            f"Clusters: {len(self.clusters)}"
+        )
 
+
+# Wrapper para 
 class Cluster:
-    def __init__(self , cluster_id):
-        pass
+    def __init__(self , cluster:container_v1.types.Cluster):
+        self.name = cluster.name
 
-    def extract_namespaces(proyect_name:str) -> list:
+    def extract_namespaces(project_name:str) -> list:
         pass
 
 class Namespace:
-    def extract_deployments(proyect_name:str) -> list:
+    def extract_deployments(project_name:str) -> list:
         pass
 
 class Deployment:
     def extract_pipeline(self):
         pass
 
-    def extract_pods(proyect_name:str) -> list:
+    def extract_pods(project_name:str) -> list:
         pass
 
     def set_config(self, config):
