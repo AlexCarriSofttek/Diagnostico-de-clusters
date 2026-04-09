9d4b1c84f13f3490876c6b770d52537b06d514b3
Edgar Alexander Carrizalez Leŕin <edgar.carrizalez@softtek.com>
2026-04-09 10:37:55 -0600

    Elementos: Proyectos
    Se pueden extraer los proyectos pero los clusters estan dando problemas porque las apis recomendads no estan habilitadas {google.cloud: container_v1 , dataproc_v1}

--- elementos.py
+++ elementos.py
@@ -1,19 +1,48 @@
+import sys
+import logging
+from google.cloud import container_v1 , dataproc_v1 
+from google.cloud import resourcemanager_v3 as resource_manager
+
 class Proyect:
-    def __init__(self , name):
-        self.name = name
+    def __init__(self , project_id):
         try:
-            self.validate_proyect()
-        except:
-            pass
+            self.project = self.open_proyect(project_id)
+            
+        except Exception as e:
+            print(f"Error al abrir proyecto: {project_id}", file=sys.stderr)
+            logging.error(f"Error al abrir proyecto: {project_id}. {e}")
+            sys.exit(1)
 
-    def validate_proyect(self):
-        pass
+    def open_proyect(self , project_id):
+        client = resource_manager.ProjectsClient()
+        project = client.get_project(name=f"projects/{project_id}")
+        return project
 
-    def extract_clusters(self , proyect_name:str) -> list:
-        pass
+    def extract_clusters(self , project_id:str , project_region:str) -> list:
+        def listar_regiones_dataproc(project_id: str) -> list[str]:
+            client = container_v1.ClusterManagerClient()
+            parent = f"projects/{project_id}/locations/-"
+
+            response = client.list_clusters()
+
+            return list({cluster.location for cluster in response.clusters})
+
+        res = []
+        for region in listar_regiones_dataproc(project_id):
+            client = dataproc_v1.ClusterControllerClient(
+                client_options={"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
+            )
+
+            clusters = client.list_clusters(
+            request={"project_id": project_id, "region": region}
+            )
+
+            res.append(clusters)
+
+        return res
 
 class Cluster:
-    def __init__(self , name):
+    def __init__(self , cluster_id):
         pass
 
     def extract_namespaces(proyect_name:str) -> list:
