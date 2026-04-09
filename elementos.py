ad864635ec7ebe10a7f40ebf26e3ef42a91b62bb
Edgar Alexander Carrizalez Leŕin <edgar.carrizalez@softtek.com>
2026-04-09 12:42:04 -0600

    Agregar Cluster
    Se agrego cluster y sus metodos para extraer namespaces

--- elementos.py
+++ elementos.py
@@ -1,12 +1,15 @@
 import sys
 import logging
+from kubernetes import client, config
 from google.cloud import container_v1 
 from google.cloud import resourcemanager_v3 as resource_manager
 
 class Project:
     def __init__(self , project_id):
         try:
-            self.project = self.open_project(project_id)
+            self._raw = self.open_project(project_id)
+            self.name = self._raw.display_name
+            self.ID = self._raw.project_id
             
         except Exception as e:
             print(f"Error al abrir project: {project_id}", file=sys.stderr)
@@ -36,31 +39,66 @@ class Project:
     def __repr__(self):
         return (
             f"Project("
-            f"project_id='{self.project.project_id}', "
-            f"display_name='{self.project.display_name}', "
-            f"state='{self.project.state.name}', "
+            f"project_id='{self._raw.project_id}', "
+            f"display_name='{self._raw.display_name}', "
+            f"state='{self._raw.state.name}', "
             f"clusters={len(self.clusters)}"
             f")"
         )
     
     def __str__(self):
         return (
-            f"Proyecto: {self.project.display_name}\n"
-            f"ID: {self.project.project_id}\n"
-            f"Estado: {self.project.state.name}\n"
+            f"Proyecto: {self._raw.display_name}\n"
+            f"ID: {self._raw.project_id}\n"
+            f"Estado: {self._raw.state.name}\n"
             f"Clusters: {len(self.clusters)}"
         )
 
-
-# Wrapper para 
+# Wrapper para cluster
 class Cluster:
     def __init__(self , cluster:container_v1.types.Cluster):
+        self._raw = cluster 
         self.name = cluster.name
+        self.location = cluster.location
+        self.status = cluster.status.name
+
+        try:
+            #config.load_incluster_config() # GCP
+            config.load_kube_config()
+            self.namespaces = self.extract_namespaces()
+            
+        except Exception as e:
+            print(f"Error al buscar namespaces", file=sys.stderr)
+            logging.error(f"Error al buscar namespaces. {e}")
+            sys.exit(1)
 
     def extract_namespaces(project_name:str) -> list:
-        pass
+        v1 = client.CoreV1Api()
+        namespaces = v1.list_namespace()
+        return [Namespace(namespace) for namespace in namespaces]
+
+    def __repr__(self):
+        return (
+            f"Cluster("
+            f"cluster_name='{self.name}', "
+            f"location='{self.location}', "
+            f"status='{self.status}', "
+            f"namespaces={len(self.namespaces)}"
+            f")"
+        )
+    
+    def __str__(self):
+        return (
+            f"Cluster: {self.name}\n"
+            f"Ubicacion: {self.location}\n"
+            f"Estatus: {self.status}\n"
+            f"Namespaces: {len(self.namespaces)}"
+        )
 
 class Namespace:
+    def __init__(self , namespace:str):
+        self._raw = namespace
+
     def extract_deployments(project_name:str) -> list:
         pass
 
